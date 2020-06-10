566b2d1571f11ba0314fa764e8920bda48a3bac6
Gaurav Shukla <55787508+iamgauravshukla@users.noreply.github.com>
2020-06-10 11:39:58 +0530

    Create robo.py

---
+++ Robo/robo.py
@@ -0,0 +1,54 @@
+import wolframalpha
+client = wolframalpha.Client("3647V2-7GHV2X2VL4")
+import wikipedia
+import PySimpleGUI as sg
+
+sg.theme('BrownBlue')
+layout = [  [sg.Text('Enter a Command '), sg.InputText()],
+            [sg.Button('Ok'), sg.Button('Cancel')] ]
+
+# Create the Window
+window = sg.Window('Robo', layout)
+
+while True:
+    event, values = window.read()
+
+    if event in (None, 'Cancel'):	# if user closes window or clicks cancel
+        break
+
+    import pyttsx3
+    engine = pyttsx3.init()
+
+    try:
+        wiki_res = wikipedia.summary(values[0], sentences=2)
+        res = client.query(values[0])
+        wolfram_res = next(res.results).text
+        sg.PopupNonBlocking("Wolfram Result : " + wolfram_res, "Wikipedia Result :" + wiki_res)
+        engine.say(wolfram_res)
+        engine.say(wiki_res)
+
+
+    except wikipedia.exceptions.DisambiguationError:
+        res = client.query(values[0])
+        wolfram_res = next(res.results).text
+        sg.PopupNonBlocking("Wolfram Result : " + wolfram_res)
+        engine.say(wolfram_res)
+
+
+    except wikipedia.exceptions.PageError:
+        res = client.query(values[0])
+        wolfram_res = next(res.results).text
+        sg.PopupNonBlocking("Wolfram Result : " + wolfram_res)
+        engine.say(wolfram_res)
+
+
+    except:
+        wiki_res = wikipedia.summary(values[0], sentences=2)
+        sg.PopupNonBlocking("Wikipedia Result : " + wiki_res)
+        engine.say(wiki_res)
+
+
+    engine.runAndWait()
+
+
+window.close()
